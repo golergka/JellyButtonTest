ffffe2a7bc90ad79f0acd544e66a258812b11c88
Max Yankov <golergka@gmail.com>
2015-01-12 22:59:55 +0200

    Basic moving mechanic

---
+++ Assets/Scripts/Moving.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Moving : MonoBehaviour
+{
+
+	void FixedUpdate ()
+	{
+		transform.position = new Vector3(
+				transform.position.x,
+				transform.position.y,
+				transform.position.z + Time.fixedDeltaTime
+			);
+	}
+}
