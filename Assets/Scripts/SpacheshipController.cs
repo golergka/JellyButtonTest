0afbdced6df17c68063ffbf1da82bf7c5aa8140b
Max Yankov <golergka@gmail.com>
2015-01-12 23:11:30 +0200

    Spaceship controlled by input

---
+++ Assets/Scripts/SpacheshipController.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpacheshipController : MonoBehaviour
+{
+	void FixedUpdate()
+	{
+		var turn = Input.GetAxis("Horizontal");
+		transform.localPosition = new Vector3(
+				transform.localPosition.x + turn * Time.fixedDeltaTime,
+				transform.localPosition.y,
+				transform.localPosition.z
+			);
+	}
+}
