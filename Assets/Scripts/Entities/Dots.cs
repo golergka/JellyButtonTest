22c274da86111f91512504983c9c75b14db6fcde
Max Yankov <golergka@gmail.com>
2015-01-14 17:58:08 +0200

    Dots

---
+++ Assets/Scripts/Entities/Dots.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent (typeof(ParticleSystem))]
+public class Dots : MonoBehaviour
+{
+	public float EmissionFactor = 10f;
+
+	void Update ()
+	{
+		particleSystem.startSpeed = Game.Instance.Speed;
+		particleSystem.emissionRate = Game.Instance.Speed * EmissionFactor;
+	}
+}
